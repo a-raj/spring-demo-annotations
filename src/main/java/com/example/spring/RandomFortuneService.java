67f7a294c89cd4224568c54a121528b1ef39ab84
a-raj <seriouswithavi@gmail.com>
2019-03-28 12:46:48 +0530

    comment

--- src/main/java/com/example/spring/RandomFortuneService.java
+++ src/main/java/com/example/spring/RandomFortuneService.java
@@ -20,6 +20,23 @@ public class RandomFortuneService implements FortuneService {
 
     private String[] fortunes = new String[3];
 
+
+    /*
+    * When Spring creates the beans it follows this general process
+        1. Construct an instance of class
+        2. Inject dependencies
+        3. Set properties etc (@Value)
+
+    In your case, when you initialized the array using this code
+
+        // create an array of strings
+        private String[] data = { a, b, c, d, e };
+
+    The Spring Bean lifecycle was at step #1 above.
+    It created an instance ... but during the assignment of the string array,
+    the properties/fields for a, b, c, d, e haven't been set yet using @Value.
+    That doesn't happen later until step #3.  So that's why you had null with the field assignment.
+    * */
     @PostConstruct
     private void setFortunes(){
         fortunes[0] = randomFortune1;
