e42f0865c0e0d67b2a4f42bde94ff511b89245d5
a-raj <seriouswithavi@gmail.com>
2019-03-29 12:39:27 +0530

    Spring Configuration with Java Code

--- src/main/java/com/example/spring/AnnotationBeanScopeDemoApp.java
+++ src/main/java/com/example/spring/AnnotationBeanScopeDemoApp.java
@@ -1,12 +1,15 @@
 package com.example.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationBeanScopeDemoApp {
 
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("../resources/applicationContext.xml");
+        // ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("../resources/applicationContext.xml");
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
         Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
 
